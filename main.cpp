4037980729f765f9aa8b6bbe07d610e0bdf47d36
matkonnerth <matkonnerth@gmail.com>
2020-02-24 19:55:07 +0100

    add threading

--- main.cpp
+++ main.cpp
@@ -7,6 +7,8 @@
 #include "MyDataSource.h"
 #include "NodeId.h"
 #include "Server.h"
+#include <thread>
+#include <chrono>
 
 void
 add(int a, int b, double c) {
@@ -54,47 +56,64 @@ NodesetLoader_unload(std::vector<std::string> s1, const std::string &s2,
     std::cout << s2 << s3 << f << std::endl;
 }
 
+opc::Server* s;
+
+void createServer()
+{
+    s = new opc::Server();    
+}
+
+void run()
+{
+    s->run();
+}
+
 int
 main() {
-
-    opc::Server s;
+    createServer();
+    std::thread serverThread{run};
+    //wild west
+    //server is accessed from multiple threads
+    using namespace std::chrono_literals;
+    std::this_thread::sleep_for(5s);
     std::vector<float> fVector{1.1f, 2.2f, 3.3f};
 
     MyDataSource source1{"source1"};
 
-    s.registerDataSource(
-        "simpleVal", getValue, setValue);
-    s.registerDataSource(
-        "vectorDataSource", getVectorValue, [](const opc::NodeId &, opc::Variant &) {});
+    s->registerDataSource("simpleVal", getValue, setValue);
+    s->registerDataSource("vectorDataSource", getVectorValue,
+                         [](const opc::NodeId &, opc::Variant &) {});
 
-    s.registerDataSource(
+    s->registerDataSource(
         source1.getKey(),
         [&](const opc::NodeId &id, opc::Variant &var) { source1.read(id, var); },
-        [&](const opc::NodeId &id, opc::Variant &var) { source1.write(id, var);});
+        [&](const opc::NodeId &id, opc::Variant &var) { source1.write(id, var); });
 
     // adding of variable nodes
-    s.addVariableNode(opc::NodeId(0, 85), opc::NodeId(1, "demoVector"), "demoVector",
+    s->addVariableNode(opc::NodeId(0, 85), opc::NodeId(1, "demoVector"), "demoVector",
                       fVector, std::make_unique<opc::NodeMetaInfo>("vectorDataSource"));
-    s.addVariableNode(opc::NodeId(0, 85), opc::NodeId(1, "demoArray"), "demoArray",
+    s->addVariableNode(opc::NodeId(0, 85), opc::NodeId(1, "demoArray"), "demoArray",
                       std::array<int, 3>{12, 13, 14});
-    s.addVariableNode(opc::NodeId(0, 85), opc::NodeId(1, "demoInt"), "demoInt", 23,
+    s->addVariableNode(opc::NodeId(0, 85), opc::NodeId(1, "demoInt"), "demoInt", 23,
                       std::make_unique<opc::NodeMetaInfo>("simpleVal"));
-    s.addVariableNode(opc::NodeId(0, 85), opc::NodeId(1, "IntVector"), "IntVector", test);
-    s.addVariableNode(opc::NodeId(0,85), opc::NodeId(1, "source1Var"), "source1Var", 12, std::make_unique<opc::NodeMetaInfo>("source1"));
+    s->addVariableNode(opc::NodeId(0, 85), opc::NodeId(1, "IntVector"), "IntVector", test);
+    s->addVariableNode(opc::NodeId(0, 85), opc::NodeId(1, "source1Var"), "source1Var", 12,
+                      std::make_unique<opc::NodeMetaInfo>("source1"));
 
     // loading of a xml nodeset
-    s.loadNodeset("../models/serviceobject.xml");
-    std::function<void(std::string)> load = [&](std::string path){s.loadNodeset(path);};
+    s->loadNodeset("../models/serviceobject.xml");
+    std::function<void(std::string)> load = [&](std::string path) {
+        s->loadNodeset(path);
+    };
 
     // bind opc ua methods to business logic
-    s.bindMethodNode(opc::NodeId(2, 7003), load);
-    s.bindMethodNode(
+    s->bindMethodNode(opc::NodeId(2, 7003), load);
+    s->bindMethodNode(
         opc::NodeId(2, 7004),
         std::function<void(std::vector<std::string>, std::string, std::string, float)>{
             NodesetLoader_unload});
 
     // not really useful now, lacks parent node id
-    s.addMethod("addMethod", &add);
-
-    s.run();
+    s->addMethod("addMethod", &add);
+    serverThread.join();
 }
