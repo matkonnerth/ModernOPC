74bc46b8cf452d757e0aef2aac0063bf0e3c1c02
matkonnerth <matkonnerth@gmail.com>
2020-03-22 08:09:14 +0100

    fix clang build

--- travis_script.sh
+++ travis_script.sh
@@ -6,7 +6,7 @@ if ! [ -z ${GCC_MEMCHECK+x} ]; then
     mkdir -p build
     cd build
     conan install -s compiler.libcxx=libstdc++11 ..
-    cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON .. 
+    cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DBUILD_SHARED_LIBS=ON .. 
     make 
     make test
     ctest --overwrite MemoryCheckCommandOptions="--leak-check=full --error-exitcode=100" -T memcheck
@@ -17,7 +17,7 @@ if ! [ -z ${USE_CLANG+x} ]; then
     mkdir -p build
     cd build
     conan install -s compiler.libcxx=libstdc++11 .. --build gtest
-    cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON .. 
+    cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DBUILD_SHARED_LIBS=ON .. 
     make 
     make test
 fi
