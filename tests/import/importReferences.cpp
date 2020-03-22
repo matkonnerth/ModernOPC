d467fc5e410911e9b24c1cd926e7c1cc60da70df
matkonnerth <matkonnerth@gmail.com>
2020-03-22 12:50:54 +0100

    add reference test

---
+++ tests/import/importReferences.cpp
@@ -0,0 +1,34 @@
+#include <Server.h>
+#include <gtest/gtest.h>
+#include <iostream>
+#include <open62541/server.h>
+
+std::string path = "";
+
+TEST(import, nodeclass)
+{
+    opc::Server server;
+    ASSERT_TRUE(server.loadNodeset(path + "/" + "importreferences.xml"));
+
+    UA_QualifiedName objectName;
+    UA_Server_readBrowseName(server.getUAServer(), UA_NODEID_NUMERIC(2,5002), &objectName);
+    ASSERT_TRUE(objectName.namespaceIndex==2);
+
+    UA_QualifiedName refName;
+    UA_Server_readBrowseName(server.getUAServer(), UA_NODEID_NUMERIC(2, 4002),
+                             &refName);
+    ASSERT_TRUE(refName.namespaceIndex == 2);
+}
+
+int main(int argc, char **argv)
+{
+
+    testing::InitGoogleTest(&argc, argv);
+
+    if (!(argc > 1))
+        return 1;
+    std::cout << argv[1] << std::endl;
+    path = argv[1];
+
+    return RUN_ALL_TESTS();
+}
