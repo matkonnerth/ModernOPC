fce18cfafac8d23c07a8f5ee2a83ec9e3ac19e11
matkonnerth <matkonnerth@gmail.com>
2020-03-22 08:03:02 +0100

    add travisScript

---
+++ travis_script.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -e
+
+# gcc, test, memcheck
+if ! [ -z ${GCC_MEMCHECK+x} ]; then
+    mkdir -p build
+    cd build
+    conan install -s compiler.libcxx=libstdc++11 ..
+    cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON .. 
+    make 
+    make test
+    ctest --overwrite MemoryCheckCommandOptions="--leak-check=full --error-exitcode=100" -T memcheck
+fi
+
+# clang, test
+if ! [ -z ${USE_CLANG+x} ]; then
+    mkdir -p build
+    cd build
+    conan install -s compiler.libcxx=libstdc++11 .. --build gtest
+    cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON .. 
+    make 
+    make test
+fi
+
