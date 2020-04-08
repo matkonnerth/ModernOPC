4bc839162a95f543d40cd90be3147ed4f8d8c70a
matkonnerth <matkonnerth@gmail.com>
2020-04-08 21:25:24 +0200

    add asan build

--- travis_script.sh
+++ travis_script.sh
@@ -7,9 +7,19 @@ if ! [ -z ${GCC_MEMCHECK+x} ]; then
     cd build
     conan install -s compiler.libcxx=libstdc++11 ..
     cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DBUILD_SHARED_LIBS=ON .. 
-    make 
+    make -j
     make test
     ctest --overwrite MemoryCheckCommandOptions="--leak-check=full --error-exitcode=100" -T memcheck
+fi
+
+# gcc + asan, test
+if ! [ -z ${GCC_ASAN+x} ]; then
+    mkdir -p build
+    cd build
+    conan install -s compiler.libcxx=libstdc++11 ..
+    cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DBUILD_SHARED_LIBS=ON -DENABLE_ASAN=ON .. 
+    make -j
+    make test
 fi
 
 # clang, test
@@ -18,7 +28,7 @@ if ! [ -z ${USE_CLANG+x} ]; then
     cd build
     conan install -s compiler.libcxx=libstdc++11 .. --build gtest
     cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DBUILD_SHARED_LIBS=ON -DCMAKE_BUILD_TYPE=Release .. 
-    make 
+    make -j
     make test
 fi
 
@@ -28,7 +38,7 @@ if ! [ -z ${COVERAGE+x} ]; then
     cd build
     conan install -s compiler.libcxx=libstdc++11 ..
     cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DBUILD_SHARED_LIBS=ON -DCALC_COVERAGE=ON .. 
-    make 
+    make -j
     make test
 fi
 
