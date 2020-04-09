e60950cc57bfe99c87316cd4e854c5e8ac7ce3bf
matkonnerth <matkonnerth@gmail.com>
2020-04-09 17:53:42 +0200

    build open in single step

--- travis_script.sh
+++ travis_script.sh
@@ -37,7 +37,8 @@ if ! [ -z ${CLANG_RELEASE+x} ]; then
     mkdir -p build
     cd build
     conan install -s compiler.libcxx=libstdc++11 .. --build libxml2 --build gtest --build benchmark
-    cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DBUILD_SHARED_LIBS=ON -DCMAKE_BUILD_TYPE=Release .. 
+    cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DBUILD_SHARED_LIBS=ON -DCMAKE_BUILD_TYPE=Release ..
+    make VERBOSE=ON open62541
     make VERBOSE=ON
     make test
 fi
