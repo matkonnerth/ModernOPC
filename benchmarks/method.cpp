ff155004efe1fd521adf146d79e91c633c3e5892
matkonnerth <matkonnerth@gmail.com>
2020-04-10 13:31:26 +0200

    cleanup cmakelists

--- benchmarks/method.cpp
+++ benchmarks/method.cpp
@@ -109,7 +109,7 @@ static void serverWrapped(benchmark::State &state)
         benchmark::DoNotOptimize(UA_Server_call(server, &req));
     }
 }
-/*
+
 int32_t manyArgs(int a0, int a1, int a2, int a3, int a4, int a5, int a6,
                   int a7, int a8, int a9, int a10, int a11, int a12, int a13,
                   int a14, int a15, int a16, int a17, int a18, int a19, int a20,
@@ -161,9 +161,9 @@ static void serverManyArgs(benchmark::State &state)
         benchmark::DoNotOptimize(UA_Server_call(server, &req));
     }
 }
-*/
+
 BENCHMARK(serverRaw);
 BENCHMARK(serverWrapped);
-//BENCHMARK(serverManyArgs);
+BENCHMARK(serverManyArgs);
 
 BENCHMARK_MAIN();
