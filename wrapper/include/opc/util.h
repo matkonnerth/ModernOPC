5102034eb355498c71e09f9da57eef90040d5285
matkonnerth <matkonnerth@gmail.com>
2020-04-11 09:31:31 +0200

    add conan

--- wrapper/include/opc/util.h
+++ wrapper/include/opc/util.h
@@ -13,4 +13,15 @@ template <class T, class Alloc>
 struct is_vector<std::vector<T, Alloc>> : public std::true_type
 {
 };
+
+template <typename T>
+struct removeConstRef
+{
+    using type = std::remove_const_t<std::remove_reference_t<T>>;
+};
+
+template<typename T>
+using typename removeConstRef_t<T> = removeConstRef<T>;
+
+
 } // namespace opc
